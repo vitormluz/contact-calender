cc0768314ce72e2573dd68a4d841860da684cb33
vitormluz <vitormdluzz2003@outlook.com>
2022-09-11 19:10:09 -0300

    image field

--- contatos/models.py
+++ contatos/models.py
@@ -17,6 +17,7 @@ class Contato(models.Model):
     criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    foto = models.ImageField(upload_to='fotos_contatos/%Y/%m/%d', blank=True, null=True)
 
     def __str__(self):
         return self.nome
